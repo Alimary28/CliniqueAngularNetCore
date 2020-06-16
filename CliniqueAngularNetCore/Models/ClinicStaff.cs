87054e5716c0049cef57030390e9f49d4e188e25
Alimary28 <alexandru28.pa@gmail.com>
2020-06-16 19:52:22 +0300

    Added ClinicStaff model and MedicalServices model.

---
+++ CliniqueAngularNetCore/Models/ClinicStaff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CliniqueAngularNetCore.Models
+{
+    public class ClinicStaff
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public DateTime EmploymentDate { get; set; }
+        public int Salary { get; set; }
+    }
+}
