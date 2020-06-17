c97fdbc9af1cdff6e1fb04a94915c85c08b5a8d9
Alimary28 <alexandru28.pa@gmail.com>
2020-06-17 16:39:46 +0300

    Add swagger and xml documentation to MedicalServices model.

--- CliniqueAngularNetCore/Controllers/MedicalServicesController.cs
+++ CliniqueAngularNetCore/Controllers/MedicalServicesController.cs
@@ -20,15 +20,16 @@ namespace CliniqueAngularNetCore.Controllers
             _context = context;
         }
 
-        // GET: api/MedicalServices
-        //[HttpGet]
-        //public async Task<ActionResult<IEnumerable<MedicalServices>>> GetMedicalServices()
-        //{
-        //    return await _context.MedicalServices.ToListAsync();
-        //}
-
+        /// <summary>
+        /// Gets a list of the medical services
+        /// </summary>
+        /// <param name="searchString">Filter services by description or type</param>
+        /// <returns>A list of medical services</returns>
+        /// <response code="200">Returns 200 if the request was succesfully completed</response>
+        
         //GET: api/MedicalServices
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<MedicalServices>>> GetMedicalServices(string searchString=null)
         {
             var result = _context.MedicalServices as IQueryable<MedicalServices>;
@@ -43,7 +44,16 @@ namespace CliniqueAngularNetCore.Controllers
             return Ok(medicalServices);
         }
         // GET: api/MedicalServices/5
+        /// <summary>
+        /// Gets the details of a medical service
+        /// </summary>
+        /// <param name="id">Search a medical service by the given id</param>
+        /// <returns>The medical service if it exists.</returns>
+        /// <response code="200">Returns 200 if the request was succesfully completed</response>
+        /// <response code="404">Returns Not Found if the object is null</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<MedicalServices>> GetMedicalServices(long id)
         {
             var medicalServices = await _context.MedicalServices.FindAsync(id);
@@ -57,9 +67,21 @@ namespace CliniqueAngularNetCore.Controllers
         }
 
         // PUT: api/MedicalServices/5
+        /// <summary>
+        /// Updates a MedicalSErvice object based on its id
+        /// </summary>
+        /// <param name="id">Search the service with the given id</param>
+        /// <param name="medicalServices">The medical service object containing all its properties</param>
+        /// <returns>A response code</returns>
+        /// <response code="204">Returns NoContent if the object was succesfully updated</response>
+        /// <response code="400">Returns a Bad Request if the id doesn't match the MedicalService id</response>
+        /// <response code="404">Returns NotFound if the MedicalService id doesn't exist</response>
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> PutMedicalServices(long id, MedicalServices medicalServices)
         {
             if (id != medicalServices.Id)
@@ -89,19 +111,35 @@ namespace CliniqueAngularNetCore.Controllers
         }
 
         // POST: api/MedicalServices
+        /// <summary>
+        /// Adds a new MedicalService object in the list 
+        /// </summary>
+        /// <param name="medicalServices">The MedicalService object with all its properties</param>
+        /// <returns>The added MedicalServices object</returns>
+        /// <response code="201">Returns the newly created item</response>
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         public async Task<ActionResult<MedicalServices>> PostMedicalServices(MedicalServices medicalServices)
         {
             _context.MedicalServices.Add(medicalServices);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetMedicalServices", new { id = medicalServices.Id }, medicalServices);
+            return CreatedAtAction(nameof(GetMedicalServices), new { id = medicalServices.Id }, medicalServices);
         }
 
         // DELETE: api/MedicalServices/5
+        /// <summary>
+        /// Removes a medical service from the list based on the given id
+        /// </summary>
+        /// <param name="id">Search for the medical service with the given id</param>
+        /// <returns>The removed MedicalServices object</returns>
+        /// <response code="404">Returns NotFound if the id doesn't exist</response>
+        /// <response code="200">Returns 200 and the removed item</response>
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<MedicalServices>> DeleteMedicalServices(long id)
         {
             var medicalServices = await _context.MedicalServices.FindAsync(id);
--- CliniqueAngularNetCore/Startup.cs
+++ CliniqueAngularNetCore/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
 using System.Reflection;
 using System.Text.Json.Serialization;
 
@@ -36,7 +39,35 @@ namespace CliniqueAngularNetCore
                 .AddFluentValidation(fv => fv.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly()));
 
             services.AddDbContext<CliniqueDbContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("CliniqueDbConnectionString")));
-           
+
+            //Register the Swagger generator, defining 1 or more Swagger documents  
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "My Clinique API",
+                    Description = "A medical clinic project for exam using ASP.NET Core",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Papp/Suciu Alina-Maria",
+                        Email = "papp_alimary@yahoo.com",
+                        Url = new Uri("https://github.com/Alimary28"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+
+                //Set the comments path for the Swagger JSON and UI
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
@@ -47,6 +78,11 @@ namespace CliniqueAngularNetCore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Clinique API v1");
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
